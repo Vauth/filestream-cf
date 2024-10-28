f33b52cb208319356d45c03609e07703a65b2a5c
Vauth <122077080+Vauth@users.noreply.github.com>
2024-10-28 14:37:43 +0330

    UPDATE bot-secret[info]

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@
 
 const BOT_TOKEN = "BOT_TOKEN"; // Insert your bot token.
 const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
-const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text.
+const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
 const BOT_OWNER = 123456789; // Insert your telegram account id.
 const BOT_CHANNEL = -100123456789; // Insert your telegram channel id which the bot is admin in.
 const SIA_NUMBER = 1234; // Insert a random integer number and keep it safe.
