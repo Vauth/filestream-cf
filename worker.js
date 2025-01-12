8bc422e3892c01b5aff9e2f2040557d9d57f4c66
Vauth <122077080+Vauth@users.noreply.github.com>
2025-01-12 14:19:58 +0330

    UPDATE info[sia-number]

--- worker.js
+++ worker.js
@@ -5,7 +5,7 @@ const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
 const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
 const BOT_OWNER = 123456789; // Insert your telegram account id.
 const BOT_CHANNEL = -100123456789; // Insert your telegram channel id which the bot is admin in.
-const SIA_NUMBER = 12345; // Insert a random integer number and keep it safe.
+const SIA_NUMBER = 12345; // Insert a random 5 digit integer number and keep it safe.
 const PUBLIC_BOT = false; // Make your bot public (only [true, false] are allowed).
 
 // ---------- Do Not Modify ---------- // 
